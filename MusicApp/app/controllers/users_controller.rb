373cc92bdaf25387b3e2c8f6503ed9f2aa675b03
joshkohane <joshkohane@gmail.com>
2020-11-03 15:12:39 -0500

    Create MusicApp with User and Session Controllers

---
+++ MusicApp/app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+    
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def show
+        @user = User.find_by_credentials(:email, :password)
+        render :show
+    end
+
+    def create
+        @user = User.new(users_params)
+
+        if @user.save
+            login!(@user)
+            redirect_to user_url(@user)
+        else
+            flash[:errors] = @user.errors.full_messages
+            redirect_to new_user_url
+        end
+    end
+
+    def users_params
+        params.require(:user).permit(:email, :password)
+    end
+end
